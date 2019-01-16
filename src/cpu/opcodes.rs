12ebe7293c87539bc46a7abc2ba88df833eb16d3
Felix Chapman <aelred717@gmail.com>
2019-01-16 00:02:54 +0000

    Improve error message on invalid opcode

--- src/cpu/opcodes.rs
+++ src/cpu/opcodes.rs
@@ -275,7 +275,7 @@ pub enum OpCode {
 
 impl OpCode {
     pub fn from_byte(byte: u8) -> Self {
-        OpCode::from_u8(byte).expect("Unrecognised opcode")
+        OpCode::from_u8(byte).unwrap_or_else(|| panic!("Unrecognised opcode: {:#x}", byte))
     }
 
     pub fn new(instr: Instruction) -> Self {
