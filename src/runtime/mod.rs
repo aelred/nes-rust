eac4474855fa06c059a7a08f3e61bb7090b94264
Felix Chapman <aelred717@gmail.com>
2024-10-07 00:30:20 +0100

    Add no-op runtime

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -22,3 +22,36 @@ const FRAME_DURATION: Duration = Duration::from_micros(1_000_000 / FPS);
 // True frequency is 1789773Hz, but tuned to match my emulator's rate
 const NES_AUDIO_FREQ: f64 = 1_866_000.0;
 const TARGET_AUDIO_FREQ: i32 = 44100;
+
+// No-op runtime when one isn't configured
+#[cfg(not(any(feature = "web", feature = "sdl")))]
+pub type ActiveRuntime = ();
+
+impl Runtime for () {
+    fn init_log(level: log::Level) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+    fn run() -> Result<(), Box<dyn Error>> {
+        use crate::{INes, NES};
+        use std::fs::File;
+
+        let args: Vec<String> = std::env::args().collect();
+
+        let ines = if let Some(filename) = args.get(1) {
+            let file = File::open(filename)?;
+            INes::read(file)?
+        } else {
+            let stdin = std::io::stdin();
+            let handle = stdin.lock();
+            INes::read(handle)?
+        };
+
+        let cartridge = ines.into_cartridge();
+
+        let mut nes = NES::new(cartridge, (), ());
+        // TODO: maybe execute indefinitely?
+        nes.tick();
+        Ok(())
+    }
+}
